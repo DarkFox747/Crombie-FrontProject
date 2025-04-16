5fa3dc40fde4c8132fede5ec41d9a1dd00debc85
DiegoS <dfsoler13@gmail.com>
2025-04-16 16:42:19 -0300

    asd

--- gym-app/next.config.ts
+++ gym-app/next.config.ts
@@ -23,15 +23,15 @@ const nextConfig: NextConfig = {
     ],
   },
   publicRuntimeConfig: {
-    GOOGLE_CLOUD_BUCKET_NAME: process.env.GOOGLE_CLOUD_BUCKET_NAME,
-    GOOGLE_CLOUD_PROJECT_ID: process.env.GOOGLE_CLOUD_PROJECT_ID,
-    DATABASE_URL: process.env.DATABASE_URL,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
       
   }, 
-
-
+  env: {    
+    DATABASE_URL: process.env.DATABASE_URL,
+    GOOGLE_CLOUD_BUCKET_NAME: process.env.GOOGLE_CLOUD_BUCKET_NAME,
+    GOOGLE_CLOUD_PROJECT_ID: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  },
 };
 
 export default nextConfig;
